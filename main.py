9977d840f918965196436066e728887b07541a4c
Don <55897579+DonDanon@users.noreply.github.com>
2020-07-06 20:01:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,16 @@
 import discord
 import os
+import config
 import random
 import asyncio
-from discord.ext import commands, tasks
-import json
-from config import PREFIX, TOKEN
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix=config.PREFIX)
+bot.remove_command('help') 
 
 
+for filename in os.listdir('./Modules'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'Modules.{filename[:-3]}')
 
-
-bot = commands.Bot(command_prefix=PREFIX)
-bot.remove_command('help')
-
-
-#Загружаю коги
-@bot.command()
-async def load(ctx, extensions):
-    bot.load_extensions(f'cogs.{extensions}')#Загрузка дополнений
-    await ctx.send('loaded')
-
-#Разгрузка
-@bot.command()
-async def reload(ctx, extensions):
-    bot.unload_extension(f'cogs.{extensions}')#Розгрузка дополнений
-    await ctx.send('unloaded')
-
-@bot.command()
-async def unload(ctx, extensions):
-    bot.unload_extension(f'cogs.{extensions}')#Розгрузка дополнений
-    bot.load_extensions(f'cogs.{extensions}')#Загрузка дополнений
-    await bot.send('unload')
-
-
-
-
-
-for filename in os.listdir('./cgs'): # Цикл перебирающий файлы в cogs
-    if filename.endswith('.py'): # если файл кончается на .py, то это наш ког
-        bot.load_extension(f'cgs.{filename[:-3]}')# загрузка дополнений
-
-
-
-
-
-
-
-bot.run(TOKEN)
+bot.run(config.TOKEN)
