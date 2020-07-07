2db57f7032e72a20f536e7f3bb4c29a43402dc43
Myrvek <66482623+Myrvek@users.noreply.github.com>
2020-07-07 15:42:19 +0300

    Update fun.py

--- Modules/fun.py
+++ Modules/fun.py
@@ -2,6 +2,7 @@
 import discord
 import config
 import random
+import sqlite3,datetime
 from discord.ext import commands
 
 
@@ -11,7 +12,15 @@ class Fun(commands.Cog):
         self.bot = bot
         self._last_member = None
         self.cog_name = ["Фановые"]
-
+    db = sqlite3.connect("Marry.db")#подклучаем бд
+	cursor = db.cursor()
+	cursor.execute("""CREATE TABLE IF NOT EXISTS marrys(
+	id1 BIGINT,
+    id2 BIGINT,
+    datem TEXT
+    )""")
+	db.commit()
+	db.close()
     @commands.command(
         aliases=["рандом", "рандомайзер", "random"],
         description="Рандомайзер",
@@ -79,6 +88,91 @@ class Fun(commands.Cog):
             e.add_field(name="Проблема:",value="Введено не цельное число!")
             e.add_field(name="Решение:",value="Введите цельное число!",inline=False)
             await ctx.send(embed=e)
+            
+    @commands.command(
+        aliases=["поженится", "marry"],
+        description="Поженится с юзером",
+        usage="поженится [Пользователь] чтобы принять написать в чат "Да" Чтобы отклнить написать в чат "Нет" ")
+	async def marry(self, ctx, user:discord.User= None):
+		db = sqlite3.connect("Marry.db") 
+		cursor = db.cursor()
+		no_one = []
+		if user != None:
+			if user == ctx.message.author:
+				await ctx.send('Вы не можете жениться на себе')
+			else:
+				cursor.execute(f"SELECT * FROM marrys WHERE id1='{ctx.message.author.id}' OR id2='{ctx.message.author.id}'")
+				res = cursor.fetchall() 
+				cursor.execute(f"SELECT * FROM marrys WHERE id1='{user.id}' OR id2='{user.id}'")
+				res1 = cursor.fetchall()
+				if not res:
+					await ctx.send(f'{user.mention}, хотите ли вы поженеться с {ctx.message.author.mention}? (Напишите : Да или Нет)')
+					try:
+						msg = await self.client.wait_for('message', timeout=300.0, check=lambda message: message.author == user)
+					except asyncio.TimeoutError:
+						await ctx.send('Увы, но время ожидание вышло.')
+						db.close()
+					else:
+						if msg.content.lower() == 'да':
+							cursor.execute(f"INSERT INTO marrys(id1, id2, datem) VALUES({ctx.message.author.id}, {user.id}, '{datetime.date.today()}')")
+							db.commit()
+							await ctx.send(f'Поздровляю! Теперь {ctx.message.author.mention} и {user.mention} женаты!')   
+							db.close()
+						elif msg.content.lower() == 'нет':
+							await ctx.send(f'{ctx.message.author.mention}, мне жаль, но вам отказали.')
+							db.close()
+				else:
+					if not res == no_one:
+						await ctx.send('Вы уже женаты')
+					elif not res1 == no_one:
+						await ctx.send('Этот пользователь уже женат.')
+				db.close()            
+		elif user == None:
+			cursor.execute(f"SELECT * FROM marrys WHERE id1='{ctx.message.author.id}' OR id2='{ctx.message.author.id}'")
+			res = cursor.fetchall()
+			if not res:
+				await ctx.send('Вы не женаты.')
+				db.close()
+			else:
+				res = res[0]
+				if res[0] == ctx.message.author.id:
+					await ctx.send(f"Вы женаты с {self.Bot.get_user(res[1])}, вы женились {res[2].replace('-', '.')}")  
+					db.close()
+				elif res[1] == ctx.message.author.id:
+					await ctx.send(f"Вы женаты с {self.Bot.get_user(res[0])}, вы женились {res[2].replace('-', '.')}")
+					db.close()
+
+
+	@commands.command(
+        aliases=["развестись", "divorce" ],
+        description="Развестить с пользователем",
+        usage="развестись [Пользователь] чтобы принять написать в чат "Да" Чтобы отклнить написать в чат "Нет" ")
+	async def divorce(self, ctx):
+		db = sqlite3.connect("Marry.db")
+		cursor = db.cursor()
+		cursor.execute(f"SELECT * FROM marrys WHERE id1='{ctx.message.author.id}' OR id2='{ctx.message.author.id}'")
+		res = cursor.fetchall()
+		if not res:
+			await ctx.send('Вы не женаты.')
+			db.close()
+		else:
+			await ctx.send(f'Вы точно хотите развестись?(Напишите : Да или Нет)')
+			try:
+				msg = await self.client.wait_for('message', timeout=10.0, check=lambda message: message.author == ctx.author)
+			except asyncio.TimeoutError:
+				await ctx.send('Увы, но время ожидание вышло.')
+				db.close()
+			else:
+				if msg.content.lower() == 'да':
+					cursor.execute(f"DELETE FROM marrys WHERE id1='{ctx.message.author.id}' OR id2='{ctx.message.author.id}'")
+					db.commit()
+					db.close()
+					await ctx.send('Вы развелись.')
+				elif msg.content.lower() == 'нет':
+					await ctx.send('Вы отменили команду.')
+					db.close()         
+            
+       
       
 
 def setup(client):
